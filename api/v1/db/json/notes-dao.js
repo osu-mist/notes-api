a7e2f77a9b8153cf6dbfdbd758845ba524a53c1b
browni <ian.brown9475@gmail.com>
2018-12-12 15:46:48 -0800

    Fix .gitignore

---
+++ api/v1/db/json/notes-dao.js
@@ -0,0 +1,46 @@
+const appRoot = require('app-root-path');
+// const _ = require('lodash');
+
+const { SerializedNotes } = require('../../serializers/notes-serializer');
+
+/**
+ * @summary Return a list of notes
+ * @function
+ * @param {Object} query Query parameters
+ * @returns {Promise} Promise object represents a list of notes
+ */
+const getNotes = query => new Promise((resolve, reject) => {
+  try {
+    const { studentID } = query;
+
+    const rawNotes = appRoot.require(`/db/${studentID}`);
+
+    const serializedNotes = SerializedNotes(rawNotes, query);
+    resolve(serializedNotes);
+  } catch (err) {
+    reject(err);
+  }
+});
+
+/**
+ * @summary Return a specific pet by unique ID
+ * @function
+ * @param {string} id Unique pet ID
+ * @returns {Promise} Promise object represents a specific pet
+ */
+// const getPetById = id => new Promise((resolve, reject) => {
+//   try {
+//     const rawPets = appRoot.require('/tests/unit/mock-data.json').pets;
+//     const rawPet = _.find(rawPets, { ID: id });
+//     if (!rawPet) {
+//       resolve(undefined);
+//     } else {
+//       const serializedPet = SerializedPet(rawPet);
+//       resolve(serializedPet);
+//     }
+//   } catch (err) {
+//     reject(err);
+//   }
+// });
+
+module.exports = { getNotes };
