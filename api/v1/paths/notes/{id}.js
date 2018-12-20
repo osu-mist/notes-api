c4835fdb6feed27e44a7e86bc4867bd150884b88
browni <ian.brown9475@gmail.com>
2018-12-20 13:29:54 -0800

    Implement DELETE /notes/{id}

--- api/v1/paths/notes/{id}.js
+++ api/v1/paths/notes/{id}.js
@@ -6,13 +6,15 @@ const { errorBuilder, errorHandler } = appRoot.require('errors/errors');
 const { openapi: { paths } } = appRoot.require('utils/load-openapi');
 const notesDAO = require('../../db/json/notes-dao');
 
+const notFoundMessage = 'A note with the specified noteID was not found.';
+
 const patch = async (req, res) => {
   try {
     const { id } = req.params;
     const { body } = req;
     const result = await notesDAO.patchNoteByID(id, body);
     if (!result) {
-      errorBuilder(res, 404, 'A note with the specified noteID was not found.');
+      errorBuilder(res, 404, notFoundMessage);
     } else {
       res.send(result);
     }
@@ -21,6 +23,21 @@ const patch = async (req, res) => {
   }
 };
 
-patch.apiDoc = paths['/notes/{noteID}'].patch;
+const del = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await notesDAO.deleteNoteByID(id);
+    if (!result) {
+      errorBuilder(res, 404, notFoundMessage);
+    } else {
+      res.status(204).send();
+    }
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
 
-module.exports = { patch };
+patch.apiDoc = paths['/notes/{noteID}'].patch;
+del.apiDoc = paths['/notes/{noteID}'].del;
+
+module.exports = { patch, del };
