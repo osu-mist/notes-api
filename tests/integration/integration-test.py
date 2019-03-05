7dfcd84808b79333a973df33578600f6433d5cab
alawammo <alawammo@oregonstate.edu>
2019-03-05 10:55:37 -0800

    integration test for notes endpoint 200 and 404

--- tests/integration/integration-test.py
+++ tests/integration/integration-test.py
@@ -21,10 +21,11 @@ class integration_tests(unittest.TestCase):
     def cleanup(cls):
         cls.session.close()
 
-    # Test case: GET /notes/{id} 
+    # Test case: GET /notes/{id}
     # Get a note by its noteID
     def test_get_notes_path(self, endpoint='/notes'):
-        for note_id in self.test_cases['valid_notes_id']:
+        # valid tests returns 200
+        for note_id in self.test_cases['valid_notes_id_in_path']:
             response = utils.make_request(self, f'{endpoint}/{note_id}', 200)
             note_schema = utils.get_resource_schema(self, 'NoteResource')
             utils.check_schema(self, response, note_schema)
@@ -32,9 +33,14 @@ class integration_tests(unittest.TestCase):
             response_data = response.json()['data']
             actual_note_id = response_data['id']
             self.assertEqual(actual_note_id, note_id)
-            
 
-    
+        # invalid tests returns 404
+        for note_id in self.test_cases['invalid_notes_id_in_path']:
+            response = utils.make_request(self, f'{endpoint}/{note_id}', 404)
+            note_schema = utils.get_resource_schema(self, 'Error')
+            utils.check_schema(self, response, note_schema)
+
+
 if __name__ == '__main__':
     arguments, argv = utils.parse_arguments()
 
