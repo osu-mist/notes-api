633f9edf329987cf86ec23203aa8a5d480f83393
alawammo <alawammo@oregonstate.edu>
2019-03-11 11:41:50 -0700

    wip query existing and none existing student id, creator id, context types

--- tests/integration/integration-test.py
+++ tests/integration/integration-test.py
@@ -37,7 +37,7 @@ class integration_tests(unittest.TestCase):
     # Get a note by its noteID
     def test_get_notes_path(self, endpoint='/notes'):
         # valid tests returns 200
-        for note_id in self.test_cases['valid_notes_id_in_path']:
+        for note_id in self.test_cases['valid_notes_id']:
             response = utils.make_request(self, f'{endpoint}/{note_id}', 200)
             schema = utils.get_resource_schema(self, 'NoteResource')
             utils.check_schema(self, response, schema)
@@ -47,14 +47,14 @@ class integration_tests(unittest.TestCase):
             self.assertEqual(actual_note_id, note_id)
 
         # invalid tests returns 404
-        for note_id in self.test_cases['invalid_notes_id_in_path']:
+        for note_id in self.test_cases['invalid_notes_id']:
             response = utils.make_request(self, f'{endpoint}/{note_id}', 404)
             schema = utils.get_resource_schema(self, 'Error')
             utils.check_schema(self, response, schema)
 
     # /notes?studentId=111111111
-    def test_get_notes_query(self, endpoint='/notes'):
-        for student_id in self.test_cases['valid_student_id_querry']:
+    def test_get_notes_query_by_student_id(self, endpoint='/notes'):
+        for student_id in self.test_cases['valid_student_id']:
             params = {'studentId': student_id}
             response = utils.make_request(self, endpoint, 200, params=params)
             note_schema = utils.get_resource_schema(self, 'NoteResource')
@@ -64,6 +64,49 @@ class integration_tests(unittest.TestCase):
                 attributes = resource['attributes']
                 actual_student_id = attributes['studentId']
                 self.assertEqual(actual_student_id, student_id)
+            self.query_creator_id(student_id)
+            self.query_context_types(student_id)
+    
+    def test_get_notes_query_non_existing_student_id(self, endpoint='/notes'):
+        for student_id in self.test_cases['non_existing_student_id']:
+            params = {'studentId': student_id}
+            response = utils.make_request(self, endpoint, 200, params=params)
+            note_schema = utils.get_resource_schema(self, 'NoteResource')
+            utils.check_schema(self, response, note_schema)
+
+        # invalid tests returns 400
+        for student_id in self.test_cases['invalid_student_id']:
+            params = {'studentId': student_id}
+            response = utils.make_request(self, endpoint, 400, params=params)
+            schema = utils.get_resource_schema(self, 'Error')
+            utils.check_schema(self, response, schema)
+
+    # /notes?studentId=111111111&creatorId
+    def query_creator_id(self, student_id, endpoint='/notes'):
+        for creator_id in self.test_cases['valid_creator_id']:
+            params = {'studentId': student_id,'creatorId': creator_id}
+            response = utils.make_request(self, endpoint, 200, params=params)
+            schema = utils.get_resource_schema(self, 'NoteResource')
+            utils.check_schema(self, response, schema)
+            # Validating the note id requested is the same note id received
+            for resource in response.json()['data']:
+                attributes = resource['attributes']
+                actual_creator_id = attributes['creatorId']
+                self.assertEqual(actual_creator_id, creator_id)
+
+    # /notes?studentId=111111111&contextTypes
+    def query_context_types(self, student_id, endpoint='/notes'):
+        for context_types in self.test_cases['valid_context_types']:
+            params = {'studentId': student_id,'contextTypes': context_types}
+            response = utils.make_request(self, endpoint, 200, params=params)
+            schema = utils.get_resource_schema(self, 'NoteResource')
+            utils.check_schema(self, response, schema)
+            # Validating the note id requested is the same note id received
+            for resource in response.json()['data']:
+                attributes = resource['attributes']
+                actual_context_types = attributes['context']
+                actual_context_types = actual_context_types['contextType']
+                self.assertEqual(actual_context_types, context_types)
 
 if __name__ == '__main__':
     arguments, argv = utils.parse_arguments()
