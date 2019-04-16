7a7087cf1e202c3efda0e2e4afcc62a938da9f0e
alawammo <alawammo@oregonstate.edu>
2019-04-16 14:48:56 -0700

    testing 400 for sortkey, creatorId and testing more than contextType and source at a time

--- tests/integration/integration-test.py
+++ tests/integration/integration-test.py
@@ -64,17 +64,17 @@ class integration_tests(unittest.TestCase):
             schema = utils.get_resource_schema(self, 'Error')
             utils.check_schema(self, response, schema)
 
-    def check_schema(self, params, endpoint='/notes'):
+    def get_response(self, params, endpoint='/notes'):
         response = utils.make_request(self, endpoint, 200, params=params)
         schema = utils.get_resource_schema(self, 'NoteResource')
         utils.check_schema(self, response, schema)
         return response
 
-    # Test case: GET /notes?studentId=
+    # Test case: GET /notes?studentId
     def test_get_notes_query_by_student_id(self, endpoint='/notes'):
         for student_id in self.test_cases['valid_student_id']:
             params = {'studentId': student_id}
-            response = self.check_schema(params)
+            response = self.get_response(params)
 
             creator_ids = []
             context_types = []
@@ -103,35 +103,37 @@ class integration_tests(unittest.TestCase):
             self.query_sort_keys(student_id)
             self.query_source(student_id, sources)
             self.query_context_type(student_id, context_types)
-
-    def test_get_notes_query_non_existing_student_id(self, endpoint='/notes'):
-        for student_id in self.test_cases['non_existing_student_id']:
-            params = {'studentId': student_id}
-            response = self.check_schema(params)
-
-        invalid_student_ids = random.sample(range(0, 99999999), 5)
-        # invalid tests returns 400
+    
+    # invalid tests returns 400
+    def test_get_notes_query_invalid_student_ids(self, endpoint='/notes'):
+        invalid_student_ids = random.sample(["12345678", "random", "1234567890", " "], 2)
         for student_id in invalid_student_ids:
             params = {'studentId': student_id}
             response = utils.make_request(self, endpoint, 400, params=params)
             schema = utils.get_resource_schema(self, 'Error')
             utils.check_schema(self, response, schema)
 
-    # /notes?studentId=111111111&creatorId
     def query_creator_id(self, student_id, creator_ids, endpoint='/notes'):
         for creator_id in creator_ids:
             params = {'studentId': student_id, 'creatorId': creator_id}
-            response = self.check_schema(params)
+            response = self.get_response(params)
             # Validating the creatorId requested is the same creatorId received
             for resource in response.json()['data']:
                 actual_creator_id = resource['attributes']['creatorId']
                 self.assertEqual(actual_creator_id, creator_id)
 
+        # invalid tests returns 400
+        invalid_creator_id = random.choice(["12345678", "random", "1234567890", " "])
+        params = {'studentId': student_id, 'creatorId': invalid_creator_id}
+        response = utils.make_request(self, endpoint, 400, params=params)
+        schema = utils.get_resource_schema(self, 'Error')
+        utils.check_schema(self, response, schema)
+
     def query_string_search(self, student_id, notes, endpoint='/notes'):
         for note in notes:
             q = random.choice(note.split(' '))
             params = {'studentId': student_id, 'q': q}
-            response = self.check_schema(params)
+            response = self.get_response(params)
             # Validating the search word requested exist in the notes received
             for resource in response.json()['data']:
                 actual_note = resource['attributes']['note']
@@ -141,7 +143,7 @@ class integration_tests(unittest.TestCase):
         sort_keys = ['lastModified', 'source', 'permissions', 'contextType']
         for sort_key in sort_keys:
             params = {'studentId': student_id, 'sortKey': sort_key}
-            response = self.check_schema(params)
+            response = self.get_response(params)
             # Validating each sort_key
             if sort_key == 'lastModified':
                 self.check_time_sort(response.json()['data'])
@@ -150,49 +152,95 @@ class integration_tests(unittest.TestCase):
             elif sort_key == 'contextType':
                 self.check_context_type_sort(response.json()['data'])
 
+        # invalid sortKey tests returns 400
+        invalid_source = random.choice([" ", "lastmodified", "random", "contexttype"])
+        params = {'studentId': student_id, 'sortKey': invalid_source}
+        response = utils.make_request(self, endpoint, 400, params=params)
+        schema = utils.get_resource_schema(self, 'Error')
+        utils.check_schema(self, response, schema)
+
     def check_time_sort(self, response):
-        last_modified = response[0]['attributes']['lastModified']
-        greater_element = datetime.strptime(last_modified,
-                                            '%Y-%m-%dT%H:%M:%S.%f%z')
-        for resource in response:
-            current_element = datetime.strptime(last_modified,
+        try:
+            last_modified = response[0]['attributes']['lastModified']
+            greater_element = datetime.strptime(last_modified,
                                                 '%Y-%m-%dT%H:%M:%S.%f%z')
-            self.assertGreaterEqual(greater_element, current_element)
-            greater_element = current_element
+            for resource in response:
+                current_element = resource['attributes']['lastModified']
+                current_element = datetime.strptime(current_element,
+                                                    '%Y-%m-%dT%H:%M:%S.%f%z')
+                self.assertGreaterEqual(greater_element, current_element)
+                greater_element = current_element
+        except IndexError:
+            self.assertEqual(response, [])
 
     def check_alphabetical_sort(self, response, sort_key):
-        greater_element = response[0]['attributes'][sort_key]
-        for resource in response:
-            current_element = resource['attributes'][sort_key]
-            self.assertGreaterEqual(current_element, greater_element)
-            greater_element = current_element
+        try:
+            greater_element = response[0]['attributes'][sort_key]
+            for resource in response:
+                current_element = resource['attributes'][sort_key]
+                self.assertGreaterEqual(current_element, greater_element)
+                greater_element = current_element
+        except IndexError:
+            self.assertEqual(response, [])
 
     def check_context_type_sort(self, response):
-        greater_element = response[0]['attributes']['context']['contextType']
-        for resource in response:
-            current_element = resource['attributes']['context']['contextType']
-            self.assertGreaterEqual(current_element, greater_element)
-            greater_element = current_element
+        try:
+            greater_element = response[0]['attributes']['context']['contextType']
+            for resource in response:
+                current_element = resource['attributes']['context']['contextType']
+                self.assertGreaterEqual(current_element, greater_element)
+                greater_element = current_element
+        except IndexError:
+            self.assertEqual(response, [])
 
     def query_source(self, student_id, sources, endpoint='/notes'):
         for source in sources:
             params = {'studentId': student_id, 'sources': source}
-            response = self.check_schema(params)
+            response = self.get_response(params)
             # Validating the source requested is the same source
             for resource in response.json()['data']:
                 actual_source = resource['attributes']['source']
                 self.assertEqual(actual_source, source)
+        # Check more than one source at a time
+        if len(sources) > 0:
+            array_len = random.randint(1, len(sources))
+            rand_sources = random.sample(sources, array_len)
+            sources = ",".join(rand_sources)
+            params = {'studentId': student_id, 'sources': sources}
+            response = self.get_response(params)
+            for resource in response.json()['data']:
+                    actual_source = resource['attributes']['source']
+                    self.assertIn(actual_source, sources)
+
+        # invalid sources tests returns 400
+        invalid_source = random.choice([" ", "advisorportal", "random", "degreeworks"])
+        params = {'studentId': student_id, 'sources': invalid_source}
+        response = utils.make_request(self, endpoint, 400, params=params)
+        schema = utils.get_resource_schema(self, 'Error')
+        utils.check_schema(self, response, schema)
 
     def query_context_type(self, student_id, context_types, endpoint='/notes'):
         for context_type in context_types:
             params = {'studentId': student_id, 'contextTypes': context_type}
-            response = self.check_schema(params)
+            response = self.get_response(params)
             # Validating the contextTypes requested
             # is the same contextTypes received
             for resource in response.json()['data']:
                 context = resource['attributes']['context']
                 actual_context_type = context['contextType']
                 self.assertEqual(actual_context_type, context_type)
+        # Check more than one contextType at a time
+        array_len = random.randint(0, len(context_types))
+        rand_context_type = random.sample(context_types, array_len)
+        context_types = ",".join(rand_context_type)
+        params = {'studentId': student_id, 'contextTypes': context_types}
+        response = self.get_response(params)
+        for resource in response.json()['data']:
+            context = resource['attributes']['context']
+            actual_context_type = context['contextType']
+            self.assertIn(actual_context_type, context_types)
+            
+
 
 
 if __name__ == '__main__':
